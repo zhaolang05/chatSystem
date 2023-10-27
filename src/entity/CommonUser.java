d7df9e23cac4ce8ad42caa258f0e17244339d95a
KerryGu <126923443+KerryGu@users.noreply.github.com>
2023-10-26 20:21:21 -0400

    Add files via upload

---
+++ src/entity/CommonUser.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class CommonUser implements User{
+    private String userName;
+    private String accountPassword;
+    private String location;
+
+    private String birthday;
+}
