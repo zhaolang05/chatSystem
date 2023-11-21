f90778f0b096dcfd4c8137aac8f91a663fb3193a
KerryGu <126923443+KerryGu@users.noreply.github.com>
2023-11-20 23:01:38 -0500

    Delete src/use_case/SignUp directory

--- src/use_case/SignUp/SignUpUserDataAccessInterface.java
+++
@@ -1,9 +0,0 @@
-package use_case.SignUp;
-
-import entity.CommonUser;
-
-public interface SignUpUserDataAccessInterface {
-    public boolean existByName(String identity);
-
-    public void save(CommonUser commonUser);
-}
