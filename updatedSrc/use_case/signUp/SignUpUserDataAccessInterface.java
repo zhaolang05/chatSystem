d113649e9d7109ec215093aba0936ffe3aa426d7
hixmyjk <126923384+hixmyjk@users.noreply.github.com>
2023-11-28 16:57:21 -0500

    Delete updatedSrc directory

--- updatedSrc/use_case/signUp/SignUpUserDataAccessInterface.java
+++
@@ -1,9 +0,0 @@
-package use_case.signUp;
-
-import entity.CommonUser;
-
-public interface SignUpUserDataAccessInterface {
-    public boolean existsByName(String identity);
-
-    public void save(CommonUser commonUser);
-}
