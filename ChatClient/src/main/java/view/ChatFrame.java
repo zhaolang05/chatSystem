5d8f57872c0e35fc45b73f276e61a976fbb1d46f
Limelime1214 <feiyang.sun@mail.utoronto.ca>
2023-12-06 00:25:02 -0500

    buttons in the chat view got modified, several json files got uploaded.

--- ChatClient/src/main/java/view/ChatFrame.java
+++ ChatClient/src/main/java/view/ChatFrame.java
@@ -211,5 +211,282 @@ public class ChatFrame extends javax.swing.JFrame implements ActionListener, Pro
         chatHistoryLabel
                 .setToolTipText("Open my chat history");
 
+        javax.swing.GroupLayout toolPanelLayout = new javax.swing.GroupLayout(
+                toolPanel);
+        toolPanel.setLayout(toolPanelLayout);
+        toolPanelLayout
+                .setHorizontalGroup(toolPanelLayout
+                        .createParallelGroup(
+                                javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(
+                                toolPanelLayout
+                                        .createSequentialGroup()
+                                        .addContainerGap()
+                                        .addComponent(
+                                                fontLabel,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                36,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(emoticonsLabel)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(sendPictureLabel)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+                                                193, Short.MAX_VALUE)
+                                        .addComponent(chatHistoryLabel)));
+        toolPanelLayout
+                .setVerticalGroup(toolPanelLayout
+                        .createParallelGroup(
+                                javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(
+                                toolPanelLayout
+                                        .createSequentialGroup()
+                                        .addGroup(
+                                                toolPanelLayout
+                                                        .createParallelGroup(
+                                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(fontLabel)
+                                                        .addComponent(emoticonsLabel)
+                                                        .addComponent(sendPictureLabel)
+                                                        .addComponent(
+                                                                chatHistoryLabel))
+                                        .addContainerGap()));
 
+        jScrollPane3.setViewportView(SendMessageTextPane);
+
+        javax.swing.GroupLayout sendPanelLayout = new javax.swing.GroupLayout(
+                sendPanel);
+        sendPanel.setLayout(sendPanelLayout);
+        sendPanelLayout
+                .setHorizontalGroup(sendPanelLayout
+                        .createParallelGroup(
+                                javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(toolPanel,
+                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                Short.MAX_VALUE)
+                        .addGroup(
+                                javax.swing.GroupLayout.Alignment.TRAILING,
+                                sendPanelLayout
+                                        .createSequentialGroup()
+                                        .addContainerGap(321, Short.MAX_VALUE)
+                                        .addComponent(
+                                                closeButton,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                76,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(
+                                                sendButton,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                77,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(5, 5, 5))
+                        .addComponent(jScrollPane3,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, 486,
+                                Short.MAX_VALUE));
+        sendPanelLayout
+                .setVerticalGroup(sendPanelLayout
+                        .createParallelGroup(
+                                javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(
+                                javax.swing.GroupLayout.Alignment.TRAILING,
+                                sendPanelLayout
+                                        .createSequentialGroup()
+                                        .addComponent(
+                                                toolPanel,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                36,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(
+                                                jScrollPane3,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                96,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(10, 10, 10)
+                                        .addGroup(
+                                                sendPanelLayout
+                                                        .createParallelGroup(
+                                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(
+                                                                sendButton,
+                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                35,
+                                                                Short.MAX_VALUE)
+                                                        .addComponent(
+                                                                closeButton,
+                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                35,
+                                                                Short.MAX_VALUE))
+                                        .addContainerGap()));
+
+        javax.swing.GroupLayout leftPanelLayout = new javax.swing.GroupLayout(
+                leftPanel);
+        leftPanel.setLayout(leftPanelLayout);
+        leftPanelLayout.setHorizontalGroup(leftPanelLayout
+                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(topPanel, javax.swing.GroupLayout.DEFAULT_SIZE,
+                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(sendPanel, javax.swing.GroupLayout.DEFAULT_SIZE,
+                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(chatPanel, javax.swing.GroupLayout.DEFAULT_SIZE,
+                        486, Short.MAX_VALUE));
+        leftPanelLayout
+                .setVerticalGroup(leftPanelLayout
+                        .createParallelGroup(
+                                javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(
+                                leftPanelLayout
+                                        .createSequentialGroup()
+                                        .addComponent(
+                                                topPanel,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                89,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(
+                                                chatPanel,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                260, Short.MAX_VALUE)
+                                        .addPreferredGap(
+                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(
+                                                sendPanel,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+                getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout
+                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(
+                        javax.swing.GroupLayout.Alignment.TRAILING,
+                        layout.createSequentialGroup()
+                                .addComponent(leftPanel,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        Short.MAX_VALUE)
+                                .addPreferredGap(
+                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(rightPanel,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)));
+        layout.setVerticalGroup(layout
+                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(rightPanel, javax.swing.GroupLayout.DEFAULT_SIZE,
+                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(leftPanel,
+                        javax.swing.GroupLayout.Alignment.TRAILING,
+                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+
+        pack();
+    }
+
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {
+        // TODO add your handling code here:
+        beforeClose();
+    }
+
+    private void SendFileLabelMouseClicked(MouseEvent evt) {
+        // TODO add your handling code here:
+        sendFile();
+    }
+
+    private void sendButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        sendMessage();
+    }
+
+    private javax.swing.JButton closeButton;
+    public javax.swing.JButton sendButton;
+    private javax.swing.JLabel showOtherLabel;
+    private javax.swing.JLabel showSelfLabel;
+
+    public javax.swing.JLabel SendFileLabel;
+    public javax.swing.JLabel sendPictureLabel;
+    private javax.swing.JLabel avatarLabel;
+    public javax.swing.JLabel fontLabel;
+    public javax.swing.JLabel myFriendLabel;
+
+    public javax.swing.JLabel chatHistoryLabel;
+    public javax.swing.JLabel emoticonsLabel;
+
+    private javax.swing.JPanel sendPanel;
+    private javax.swing.JPanel rightPanel;
+    private javax.swing.JPanel leftPanel;
+    private javax.swing.JPanel chatPanel;
+    private javax.swing.JPanel topPanel;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane3;
+    public javax.swing.JTextPane SendMessageTextPane;
+    public javax.swing.JTextPane receiveTextPane;
+
+
+    public javax.swing.JTextPane getReceiveTextPane() {
+        return receiveTextPane;
+    }
+
+    public void setReceiveTextPane(javax.swing.JTextPane receiveTextPane) {
+        this.receiveTextPane = receiveTextPane;
+    }
+
+    /**
+     * Send a message, an empty method, implemented by a subclass
+     */
+    public void sendMessage() {
+    }
+
+    public void beforeClose() {
+    }
+
+    /**
+     * Select an emote
+     */
+    public void selectFace() {
+    }
+
+    /**
+     * deal emoticons in messages
+     */
+    public void dealIcon(String str) {
+
+    }
+
+    /**
+     * send file
+     */
+    public void sendFile() {
+
+    }
+
+
+    /**
+     * send picture
+     */
+    public void sendImg() {
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
